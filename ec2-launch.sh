3bc733a8897c14c6cba47ef229b0019858771bcc
Shiju Raj <shijuraj.reny@gmail.com>
2022-10-23 21:12:13 -0400

    created jason file to create spot instance

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,4 +3,4 @@
 #1. Create a Spot Instance
 #2. Take that Instance IP & register DNS
 
-aws ec2 request-spot-instances  --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=Frontend}]"
+aws ec2 request-spot-instances  --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instance-request, Tags=[{Key=Name,Value=Frontend}]"
