ab1cc392a77b97b6c7614674a24c781fc4c62513
Shiju Raj <shijuraj.reny@gmail.com>
2022-10-26 22:14:02 -0400

    Tag added

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,4 +3,4 @@
 #1. Create a Spot Instance
 #2. Take that Instance IP & register DNS
 
-aws ec2 request-spot-instances  --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request, Tags=[{Key=Name,Value=Frontend}]" "ResourceType=instance, Tags=[{Key=Name,Value=Frontend}]"
+aws ec2 request-spot-instances  --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=Frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=Frontend}]"
