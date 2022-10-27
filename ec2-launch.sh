5d3c3be35211ca2dac908397dc5fea43d2d40a59
Shiju Raj <shijuraj.reny@gmail.com>
2022-10-26 23:10:31 -0400

    Tag added

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -2,7 +2,7 @@
 
 #1. Create a Spot Instance
 #2. Take that Instance IP & register DNS
-#L_ID: "05589047b6c5587a2"
-#Version: 4
+L_ID: "05589047b6c5587a2"
+Version: 4
 
-aws ec2 run-instances  --launch-template  --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=Frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=Frontend}]"
+aws ec2 run-instances  --launch-template LaunchTemplateId=${L_ID} Version=${Version} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=Frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=Frontend}]"
