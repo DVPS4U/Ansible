6d0365e107b545d995f5993a0ede104522d4e6b5
Shiju Raj <shijuraj.reny@gmail.com>
2022-10-26 23:16:09 -0400

    version issue correction

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,6 +3,6 @@
 #1. Create a Spot Instance
 #2. Take that Instance IP & register DNS
 L_ID="lt-05589047b6c5587a2"
-V=2
+V=4
 
-aws ec2 run-instances  --launch-template LaunchTemplateId=${L_ID} Version=${V} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=Frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=Frontend}]"
+aws ec2 run-instances  --launch-template LaunchTemplateId=${L_ID},Version=${V} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=Frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=Frontend}]"
